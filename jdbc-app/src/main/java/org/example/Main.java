e0faba7091cf38e97dbb05ac9c6da809328928a5
Samuel Estrella <sammye70@gmail.com>
2024-04-12 03:52:51 -0600

    create basic structure of the project - using Maven

---
+++ jdbc-app/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
